f5d005cf57015258e21bdf8091591920b8a3eb8c
syokoi <syokoi@WP20104.wing.sysrdc.com>
2021-07-05 11:55:00 +0900

    add py from 3090_PC

--- numpyro_RTX3080/run_bash.sh
+++ numpyro_RTX3080/run_bash.sh
@@ -1,4 +1,4 @@
-## イメージ作成  sinpcw/pytorch:1.8.0 を使うとgpuのjaxは使えた
+# イメージ作成  sinpcw/pytorch:1.8.0 を使うとgpuのjaxは使えた
 docker build -t pytorch_numpyro_gpu -f pytorch18.Dockerfile .
 
 # コンテナ起動してbashで入る
@@ -7,7 +7,7 @@ docker run -p 8888:8888 -p 8502:8502 -it -v $PWD/work:/work --rm --gpus all pyto
 ## gpu 確認
 # python -c "import torch, jax, numpyro; print(torch.cuda.is_available()); print(jax.devices())"
 ## True
-## [CpuDevice(id=0)] / [GpuDevice(id=0)]
+## [GpuDevice(id=0)]
 
 ## jax, numpyro動くか確認
 # python ./jax_numpyro_test.py
